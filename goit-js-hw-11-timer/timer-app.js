234c7a0d86f39cc6a025c0d660ea6218e70275ee
Nick_Khoroshkov <nickolaykhoroshkov@gmail.com>
2019-11-26 15:51:48 +0200

    timer-v.01

---
+++ goit-js-hw-11-timer/timer-app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
+
+  updateTimerInterface() {
+    const time = Date.parse(this.targetDate) - Date.now();
+
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+    document.querySelector('[data-value="days"]').textContent = `${days}`;
+    document.querySelector('[data-value="hours"]').textContent = `${hours}`;
+    document.querySelector('[data-value="mins"]').textContent = `${mins}`;
+    document.querySelector('[data-value="secs"]').textContent = `${secs}`;
+
+    setInterval(() => {
+
+      this.updateTimerInterface();
+    }, 1000);
+  }
+}
+
+
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Nov 29, 2019'),
+});
+
+timer.updateTimerInterface();
